2ab773aa395c2b8b3cdb0199654d1230d9fc693a
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2020-02-05 23:30:39 +0100

    Add files via upload
    Nisam uspio ovaj rijesit cijeli

---
+++ klasaArraylist/klasaArraylist/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace klasaArraylist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonUnesi = new System.Windows.Forms.Button();
+            this.buttonNovi = new System.Windows.Forms.Button();
+            this.textBoxNovi = new System.Windows.Forms.TextBox();
+            this.buttonObrisi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(108, 164);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(315, 211);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonUnesi
+            // 
+            this.buttonUnesi.Location = new System.Drawing.Point(348, 112);
+            this.buttonUnesi.Name = "buttonUnesi";
+            this.buttonUnesi.Size = new System.Drawing.Size(75, 23);
+            this.buttonUnesi.TabIndex = 1;
+            this.buttonUnesi.Text = "Unesi";
+            this.buttonUnesi.UseVisualStyleBackColor = true;
+            // 
+            // buttonNovi
+            // 
+            this.buttonNovi.Location = new System.Drawing.Point(119, 56);
+            this.buttonNovi.Name = "buttonNovi";
+            this.buttonNovi.Size = new System.Drawing.Size(214, 23);
+            this.buttonNovi.TabIndex = 2;
+            this.buttonNovi.Text = "Novi automobil";
+            this.buttonNovi.UseVisualStyleBackColor = true;
+            this.buttonNovi.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxNovi
+            // 
+            this.textBoxNovi.Location = new System.Drawing.Point(119, 112);
+            this.textBoxNovi.Name = "textBoxNovi";
+            this.textBoxNovi.Size = new System.Drawing.Size(214, 20);
+            this.textBoxNovi.TabIndex = 3;
+            // 
+            // buttonObrisi
+            // 
+            this.buttonObrisi.Location = new System.Drawing.Point(108, 407);
+            this.buttonObrisi.Name = "buttonObrisi";
+            this.buttonObrisi.Size = new System.Drawing.Size(75, 23);
+            this.buttonObrisi.TabIndex = 4;
+            this.buttonObrisi.Text = "Obrisi listu";
+            this.buttonObrisi.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonObrisi);
+            this.Controls.Add(this.textBoxNovi);
+            this.Controls.Add(this.buttonNovi);
+            this.Controls.Add(this.buttonUnesi);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonUnesi;
+        private System.Windows.Forms.Button buttonNovi;
+        private System.Windows.Forms.TextBox textBoxNovi;
+        private System.Windows.Forms.Button buttonObrisi;
+    }
+}
+
